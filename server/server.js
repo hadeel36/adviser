be6e9cc05df58debc0664644977056833aabf7c5
sarya-alsayed <e.saryaa@gmail.com>
2016-12-04 13:21:47 +0200

    main folders

---
+++ server/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var mongoose = require('mongoose');
+
+var app = express();
+
+// connect to mongo database named "shortly"
+mongoose.connect('mongodb://localhost/adviser');
+
+// configure our server with all the middleware and routing
+require('./config/middleware.js')(app, express);
+require('./config/routes.js')(app, express);
+
+// start listening to requests on port 8000
+app.listen(8000);
+console.log("server start at http://127.0.0.1:8000/")
+
+// export our app for testing and flexibility, required by index.js
+module.exports = app;
