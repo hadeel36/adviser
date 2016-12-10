f44bd9e5de86bfb1676434df3971fb8ed5cfd3bd
sarya-alsayed <e.saryaa@gmail.com>
2016-12-10 12:20:35 +0200

    jordanPlace controller

--- server/jordan/jordanController.js
+++ server/jordan/jordanController.js
@@ -15,5 +15,18 @@ module.exports = {
 		.exec(function (err, jordan) {
 			repsonseHandler(err, req, res, {status: 201, returnObj: jordan}, next);
 		});
+	},
+	createJordan: function (req, res, next) {
+		var description = req.body.description;
+		var mainPhoto = req.body.mainPhoto;
+		var mapPhoto = req.body.mapPhoto;
+		var newJordan = new Jordan({
+			description: description,
+			mainPhoto: mainPhoto,
+			mapPhoto: mapPhoto
+		});
+		newJordan.save(function (err, jordan) {
+			repsonseHandler(err, req, res, {status: 201, returnObj: jordan}, next);
+		});
 	}		
 };
