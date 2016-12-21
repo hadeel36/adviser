66bb586e6e999e8851f11782519178ffd7645cca
sarya-alsayed <e.saryaa@gmail.com>
2016-12-21 20:40:00 +0200

    design packages

--- client/app/package/package.js
+++ client/app/package/package.js
@@ -1,10 +1,11 @@
 angular.module('adviser.package', [])
-.controller('packageController', function($scope, $routeParams, Package){
+.controller('packageController', function($scope, $routeParams, Package, $location){
 	
 	var getAllPackages= function(){
 		Package.getPackages($routeParams.type)
 		.then(function(packages){
 			$scope.data= packages;
+			console.log($location.path);
 			console.log($scope.data);
 		}).catch(function(error){
 			alert("an error occured");
