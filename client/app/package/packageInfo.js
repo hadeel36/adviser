616b51bbffbc7103b1886981e784cc68d39ba222
hadeel36 <hadylgk87@gmail.com>
2017-01-10 06:45:31 +0200

    convert to https -not working-

--- client/app/package/packageInfo.js
+++ client/app/package/packageInfo.js
@@ -1,5 +1,6 @@
 angular.module('adviser.packageInfo', [])
-.controller('packageInfoController', function($scope, $routeParams, Package){
+.controller('packageInfoController', function($scope, $routeParams, Package, $mdToast, $http){
+
     $scope.myInterval = 3000;
     $scope.total = 0;
     $scope.sgl = 0;
@@ -171,4 +172,45 @@ angular.module('adviser.packageInfo', [])
 		})
 
 	}
+	$scope.toastPosition = {
+	          bottom: false,
+	          top: true,
+	          left: false,
+	          right: true
+	      };
+ $scope.getToastPosition = function () {
+      return Object.keys($scope.toastPosition)
+          .filter(function (pos) {
+              return $scope.toastPosition[pos];
+          })
+          .join(' ');
+  };
+
+	$scope.sendMail= function(){
+       var data= ({
+           destinationName : 'hiiiiiiiiiiiiii',
+           contactEmail : "hadylgk87@gmail.com",
+           contactMsg : 'hiiiiiiiiiiiiiiiiiiiiiiii'
+       });
+		
+       // Simple POST request example (passing data) :
+       $http.post('/api/send_Mail', data).
+           success(function(data, status, headers, config) {
+               // this callback will be called asynchronously
+               // when the response is available
+               console.log(data);
+               $mdToast.show(
+                   $mdToast.simple()
+                       .content('Thanks for your message ' + data.destinationName + ' You Rock!')
+                       .position($scope.getToastPosition())
+                       .hideDelay(5000)
+               );
+
+           }).
+           error(function(data, status, headers, config) {
+               // called asynchronously if an error occurs
+               // or server returns response with an error status.
+           });
+
+	};
 });
