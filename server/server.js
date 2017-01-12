20415a6f72b9e7c079dd21fcda8a6f309b6e4faa
hadeel36 <hadylgk87@gmail.com>
2017-01-12 06:20:19 +0200

    fix conflict

--- server/server.js
+++ server/server.js
@@ -6,11 +6,23 @@ var mongoose = require('mongoose');
 var autoIncrement = require('mongoose-auto-increment');
 
 var app = express();
+<<<<<<< e078e75d1c9a6c6ddb1ca2d79a69acc7fe5b4ea0
 var mongoURL = process.env.MONGODB_URI || 'mongodb://localhost/adviser';
 
 
+=======
+var mongoURL = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017?ssl=true';
+
+var mongoOpt = {
+      "server": { 
+        "sslValidate": false,
+        "sslKey": fs.readFileSync('mongodb.pem'),
+        "sslCert": fs.readFileSync('mongodb-cert.key')
+      }
+    }
+>>>>>>> fix conflict
 // connect to mongo database named "shortly"
-var connection = mongoose.connect(mongoURL);
+var connection = mongoose.connect(mongoURL, mongoOpt);
 autoIncrement.initialize(connection);
 
 
@@ -18,11 +30,24 @@ autoIncrement.initialize(connection);
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
+<<<<<<< e078e75d1c9a6c6ddb1ca2d79a69acc7fe5b4ea0
 
 
+=======
+var sslkey = fs.readFileSync('./key.pem');
+var sslcert = fs.readFileSync('./cert.pem')
+
+var options = {
+    key: sslkey,
+    cert: sslcert
+};
+
+https.createServer(options, app).listen(process.env.PORT || 8000);
+console.log("server start at http://127.0.0.1:8000/");
+>>>>>>> fix conflict
 // start listening to requests on port 8000
-app.listen(process.env.PORT || 8000);
-console.log("server start at http://127.0.0.1:8000/")
+// app.listen(process.env.PORT || 8000);
+// console.log("server start at http://127.0.0.1:8000/")
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
