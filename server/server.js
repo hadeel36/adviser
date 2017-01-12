75e6dec348e6e40a88b5fc309622638e6160a50f
hadeel36 <hadylgk87@gmail.com>
2017-01-12 06:15:30 +0200

    the server working

--- server/server.js
+++ server/server.js
@@ -8,15 +8,9 @@ var autoIncrement = require('mongoose-auto-increment');
 var app = express();
 var mongoURL = process.env.MONGODB_URI || 'mongodb://localhost/adviser';
 
-var mongoOpt = {
-      "server": { 
-        "sslValidate": false,
-        "sslKey": fs.readFileSync('mongodb.pem'),
-        "sslCert": fs.readFileSync('mongodb-cert.key')
-      }
-    }
+
 // connect to mongo database named "shortly"
-var connection = mongoose.connect(mongoURL, mongoOpt);
+var connection = mongoose.connect(mongoURL);
 autoIncrement.initialize(connection);
 
 
@@ -24,19 +18,11 @@ autoIncrement.initialize(connection);
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
-var sslkey = fs.readFileSync('./key.pem');
-var sslcert = fs.readFileSync('./cert.pem')
 
-var options = {
-    key: sslkey,
-    cert: sslcert
-};
 
-https.createServer(options, app).listen(process.env.PORT || 8000);
-console.log("server start at http://127.0.0.1:8000/");
 // start listening to requests on port 8000
-// app.listen(process.env.PORT || 8000);
-// console.log("server start at http://127.0.0.1:8000/")
+app.listen(process.env.PORT || 8000);
+console.log("server start at http://127.0.0.1:8000/")
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
