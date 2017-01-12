c2792e5d8cdcc61989da38e2699b9fdd762e89a8
hadeel36 <hadylgk87@gmail.com>
2017-01-12 06:15:30 +0200

    the server working

--- server/server.js
+++ server/server.js
@@ -8,8 +8,12 @@ var autoIncrement = require('mongoose-auto-increment');
 var app = express();
 var mongoURL = process.env.MONGODB_URI || 'mongodb://localhost/adviser';
 
+<<<<<<< 1210d2ee4d943f0f91ec29256e6429dcdab28507
+=======
+
+>>>>>>> the server working
 // connect to mongo database named "shortly"
-var connection = mongoose.connect(mongoURL, mongoOpt);
+var connection = mongoose.connect(mongoURL);
 autoIncrement.initialize(connection);
 
 
@@ -17,9 +21,14 @@ autoIncrement.initialize(connection);
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
+<<<<<<< 1210d2ee4d943f0f91ec29256e6429dcdab28507
+=======
+
+
+>>>>>>> the server working
 // start listening to requests on port 8000
-// app.listen(process.env.PORT || 8000);
-// console.log("server start at http://127.0.0.1:8000/")
+app.listen(process.env.PORT || 8000);
+console.log("server start at http://127.0.0.1:8000/")
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
