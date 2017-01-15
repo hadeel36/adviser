f94957cda9f252dcbd0d608f80f794008890e133
hadeel36 <hadylgk87@gmail.com>
2017-01-15 06:14:20 +0200

    add about us and contact us

--- server/config/routes.js
+++ server/config/routes.js
@@ -30,6 +30,7 @@ app.get('/api/destination/destinationInfo/:id', destinationController.destinatio
 app.post('/api/upload',utils.uploadImg);
 // send mail
 app.post('/api/send_Mail',utils.sendMail);
+app.post('/api/submit_Mail',utils.submitMail);
   // If a request is sent somewhere other than the routes above,
   // send it through our custom error handler
 app.use(helpers.errorLogger);
